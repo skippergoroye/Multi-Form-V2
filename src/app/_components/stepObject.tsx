5b892852e42f76d0f885f9f5cfb58d672a211949
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-05-19 08:38:47 +0100

    feat: step color now changing

--- src/app/_components/stepObject.tsx
+++ src/app/_components/stepObject.tsx
@@ -1,33 +1,24 @@
 import React from "react";
 
-
-
 export type StepObjectType = {
-    id: number;
-    title: string;
-    description: string;
-
-}
+  id: number;
+  title: string;
+  description: string;
+};
 interface Props {
-    step: number;
-    data: StepObjectType[];
+  step: number;
+  data: StepObjectType[];
 }
 
-const StepObject = ({step, data }: Props) => {
+const StepObject = ({ step, data }: Props) => {
   return (
     <section className="grid grid-cols-3 text-sm gap-6 mb-10 ">
-      <article>
-        <div className="h-2 w-3/4 bg-slate-800" />
-        Personal Information
-      </article>
-      <article>
-        <div className="h-2 w-3/4 bg-slate-800" />
-        Account Information
-      </article>
-      <article>
-        <div className="h-2 w-3/4 bg-slate-800" />
-        Passport Information
-      </article>
+      {data.map((item, index) => (
+        <article key={index} className={`text-sm ${step >= item.id ? "text-emerald-600": ""}`}>
+           <div className={`h-2 w-3/4 ${step >= item.id ? "bg-emerald-600" : "bg-slate-800"}`} />
+          {item.title}
+        </article>
+      ))}
     </section>
   );
 };
